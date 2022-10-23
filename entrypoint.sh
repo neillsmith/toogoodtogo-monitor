358c27f536c26d310a15b141132e0c480ce0b1db
neillsmith <smith.neill@gmail.com>
2022-10-23 17:20:11 +0100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,1 +0,0 @@
-npm start -- --email smith.neill+toogoodtogo@gmail.com --discord https://discord.com/api/webhooks/1032645837202927687/ROHKQUo4LmFwolcEMjKHeGOzTvbpeepzzT8ys5CKF3897_UqO5DFXObhbALmdtKHrMgv
